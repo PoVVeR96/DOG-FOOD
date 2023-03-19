08e30a57fd7ee237dbfc8fc6e6b7e624361d6412
Nikita <poverinov94@mail.ru>
2023-03-19 14:23:52 +0300

    dogfood

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import React, { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { App } from "./components/app";
+// // import { AppMui } from './components/app-mui';
+// import "./styles.css";
+// // import { AntApp } from './components/app-ant/index';
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(<App/>);
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./components/app";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
